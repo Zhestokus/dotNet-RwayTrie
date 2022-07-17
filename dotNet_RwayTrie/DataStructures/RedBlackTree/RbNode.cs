f468c19ba3e0a381cf96a38ba80cca1bad2d86c4
Zhestokus <v.ezugbaia@gmail.com>
2022-07-17 23:51:11 +0400

    Add files via upload

---
+++ dotNet_RwayTrie/DataStructures/RedBlackTree/RbNode.cs
@@ -0,0 +1,28 @@
+﻿namespace dotNet_RwayTrie.DataStructures.RedBlackTree
+{
+    public class RbNode<TKey, TVal>
+    {
+        public TKey Key;
+        public TVal Val;
+
+        public bool IsRed;
+
+        public RbNode<TKey, TVal> Left;
+        public RbNode<TKey, TVal> Right;
+
+        public RbNode(TKey key, TVal val)
+        {
+            Key = key;
+            Val = val;
+            IsRed = true;
+        }
+
+        public RbNode(TKey key, TVal val, bool isRed)
+        {
+            Key = key;
+            Val = val;
+            IsRed = isRed;
+        }
+    }
+
+}
---
+++ dotNet_RwayTrie/DataStructures/RwayTrieStd/RWayNodeStd.cs
@@ -0,0 +1,12 @@
+﻿namespace dotNet_RwayTrie.DataStructures.RWayTrieStd
+{
+    public class RWayNodeStd<TVal>
+    {
+        public byte Key;
+        public TVal Val;
+
+        public bool IsLeaf;
+
+        public RWayNodeStd<TVal>[] Children;
+    }
+}
